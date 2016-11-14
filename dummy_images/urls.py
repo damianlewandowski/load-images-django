dbe536f967b8999b819fc232cbf63528879a545a
Cherry79 <Cherry79>
2016-11-14 19:56:44 +0100

    Cache left

--- dummy_images/urls.py
+++ dummy_images/urls.py
@@ -6,5 +6,5 @@ from . import views
 app_name = 'dummy_images'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<resolution>[0-9]+x[0-9]+)/$', views.images, name='images')
+    url(r'^(?P<resolution>[0-9]+x[0-9]+)/$', views.ImageView.as_view(), name='images')
 ]
--- dummy_images/views.py
+++ dummy_images/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.views.generic import ListView
 import random
 
 from .models import DummyImage
 
 
-def images(request, resolution):
-    """Split resolution string by 'x' character."""
-    width = resolution.split('x')[0]
-    height = resolution.split('x')[1]
+class ImageView(ListView):
+    model = DummyImage
+    template_name = 'dummy_images/images.html'
 
-    dummy_images = DummyImage.objects.all()
-    dummy_images_counter = DummyImage.objects.all().count()
-    random_image = dummy_images[random.randrange(0, dummy_images_counter)]
+    def get_context_data(self, **kwargs):
+        """Split resolution string by 'x' character."""
+        width = self.kwargs['resolution'].split('x')[0]
+        height = self.kwargs['resolution'].split('x')[1]
 
-    return render(request, 'dummy_images/images.html', {
-        'width': width,
-        'height': height,
-        'img': random_image,
-    })
+        dummy_images = DummyImage.objects.all()
+        dummy_images_counter = DummyImage.objects.all().count(),
+        
+        random_image = get_object_or_404(DummyImage, pk=dummy_images[random.randrange(0, dummy_images_counter)].id)
+        context = {'width': width, 'height': height, 'img': random_image, 'test': 'test'}
+
+        return context
 
 
 def index(request):
